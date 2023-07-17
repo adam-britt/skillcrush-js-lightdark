3d94a20fdb743cfcd6ff78cb2223ebb72f8959ea
Adam Britt <adamrussellbritt@gmail.com>
2023-07-17 09:39:34 -0500

    added all files

---
+++ js/script.js
@@ -0,0 +1,28 @@
+var fans = document.querySelector(".fans");
+var fish = document.querySelector(".fish");
+var pets = document.querySelector(".pets");
+
+fans.addEventListener("mouseover", function() {
+    fans.innerText = "123K";
+});
+
+fish.addEventListener("mouseover", function() {
+    fish.innerText = "47K";
+});
+
+pets.addEventListener("mouseover", function() {
+    pets.innerText = "20K";
+});
+
+
+
+var toggle = document.querySelector("#toggle");
+var ball = document.querySelector(".ball");
+var h1 = document.querySelector("h1");
+var body = document.querySelector("body");
+
+if(toggle.addEventListener("click", function() {
+
+    
+
+}));
