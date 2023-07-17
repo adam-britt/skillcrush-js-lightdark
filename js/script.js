be8436a11858bfeb0ae904a5b9434a8d26233344
Adam Britt <adamrussellbritt@gmail.com>
2023-07-17 10:08:57 -0500

    final commit

--- js/script.js
+++ js/script.js
@@ -21,8 +21,31 @@ var ball = document.querySelector(".ball");
 var h1 = document.querySelector("h1");
 var body = document.querySelector("body");
 
-if(toggle.addEventListener("click", function() {
+toggle.addEventListener("click", function() {
 
-    
+    if(body.classList.contains("light")) {
+        body.classList.remove("light");
+        ball.classList.remove("move-right");
+        h1.innerHTML = "Stealth Quincy";
+    } else {
+        body.classList.add("light");
+        ball.classList.add("move-right");
+        h1.innerHTML = "Party Quincy";
+    }
 
-}));
+});
+
+
+
+var modal = document.querySelector(".modal");
+var modalX = document.querySelector(".modal-x");
+
+modal.addEventListener("click", function() {
+    if(modal.classList.contains("show-modal")) {
+        modalX.classList.add("show-modal");
+        modal.innerText = "You've got this!";
+    } else {
+        modalX.classList.remove("show-modal");
+        modal.innterText = "Who's got this?";
+    }
+});
